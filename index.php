1fe735e38eb47bb1ee28cb477c471e1f0f2cfe2c
Rado <radovan.smitala@gmail.com>
2021-02-15 10:32:58 +0100

    Version up

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       C3W base helpers
  * Description:       Fundamental helpers
- * Version:           1.2.0
+ * Version:           1.2.1
  * Requires at least: 5.2
  * Requires PHP:      7.3
  * Text Domain:       base-helpers
