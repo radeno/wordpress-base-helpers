44f4550956bf521321fc2078ec6439a0d1cb5c12
Rado <radovan.smitala@gmail.com>
2021-03-03 22:54:57 +0100

    Fix issue with menu ancestor

--- inc/MenuHelper.php
+++ inc/MenuHelper.php
@@ -20,50 +20,39 @@ class MenuHelper
                 return $item;
             };
 
-            // Add active class when is single post type and archive is in menu
-            $changedParentIds = [];
-            $newSortedItems = array_map(function ($item) use ($setActiveItem, &$changedParentIds) {
+            $addedParentIds = array_unique(array_map(function ($item) {
+                if (!\is_singular()) {
+                    return null;
+                }
+
                 if ($item->type === 'post_type_archive' && \get_post_type() === $item->object) {
-                    $item = $setActiveItem($item);
-                    $changedParentIds[] = $item->menu_item_parent;
+                    return $item->menu_item_parent;
                 }
 
                 if ($item->type === 'taxonomy') {
-                    $postTermsIds = \wp_get_post_terms(get_the_ID(), $item->object, ['fields' => 'ids']);
+                    $postTermsIds = \wp_get_post_terms(\get_the_id(), $item->object, ['fields' => 'ids']);
                     if (! \is_wp_error($postTermsIds) && in_array($item->object_id, $postTermsIds)) {
-                        $item = $setActiveItem($item);
-                        $changedParentIds[] = $item->menu_item_parent;
+                        return $item->menu_item_parent;
                     }
+                }
+
+                if ($item->type === 'post_type') {
+                    $ancestorsIds = \get_post_ancestors(\get_the_id());
+                    if (!empty($ancestorsIds)) {
+                        if (in_array((int)$item->object_id, $ancestorsIds)) {
+                            return $item->ID;
+                        }
+                    }
+                }
+            }, $sorted_menu_items));
+
+            return array_map(function ($item) use ($setActiveItem, $addedParentIds) {
+                if (in_array($item->ID, $addedParentIds)) {
+                    $item = $setActiveItem($item);
                 }
 
                 return $item;
             }, $sorted_menu_items);
-
-            // Add all parents of active post type achive
-            $newSortedItems = array_map(function ($item) use ($setActiveItem, &$changedParentIds) {
-                if (in_array($item->object_id, $changedParentIds)) {
-                    $item = $setActiveItem($item);
-                }
-
-                return $item;
-            }, $newSortedItems);
-
-            // Add active to all parents
-            if (\is_singular()) {
-                $ancestorsIds = \get_post_ancestors(\get_the_id());
-
-                if (!empty($ancestorsIds)) {
-                    $newSortedItems = array_map(function ($item) use ($setActiveItem, $ancestorsIds) {
-                        if ($item->type === 'post_type' && in_array((int)$item->object_id, $ancestorsIds)) {
-                            $item = $setActiveItem($item);
-                        }
-
-                        return $item;
-                    }, $newSortedItems);
-                }
-            }
-
-            return $newSortedItems;
         }, 10, 2);
     }
 }
