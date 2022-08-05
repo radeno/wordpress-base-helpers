554e67bdfc7fa031c15ef1944212a645ec84ea47
Radovan Šmitala <radovan.smitala@gmail.com>
2022-08-05 08:51:37 +0200

    version 1.3.3

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       C3W base helpers
  * Description:       Fundamental helpers
- * Version:           1.3.0
+ * Version:           1.3.3
  * Requires at least: 5.2
  * Requires PHP:      7.3
  * Text Domain:       base-helpers
